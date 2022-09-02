5c1ab1752d1c5eb0438948a0b9af3e6f1d10edb6
Lorenzo Greco <lorenzo.rev@gmail.com>
2022-09-02 13:59:33 +0200

    update bstack options, fix scripts

--- lib/scripts/e2e.browserstack.android.sh
+++ lib/scripts/e2e.browserstack.android.sh
@@ -22,12 +22,12 @@ then
   echo "$(yellow 🔧 upload .apk)"
 
   RESULT=$(curl -u "$BS_USER:$BS_KEY" -X POST "https://api-cloud.browserstack.com/app-automate/upload" -F "file=@$ANDROID_FOLDER_PATH/app/build/outputs/apk/$ANDROID_TARGET/release/app-$ANDROID_TARGET-release-unsigned.apk")
-
-  export BS_PATH=$(node -pe 'JSON.parse(process.argv[1]).app_url' $RESULT)
+  
+  export BS_PATH=$(node -pe 'JSON.parse(process.argv[1]).app_url' "$RESULT")
 
   if [ "$2" == "--spec" ]; then
       FIX_SPEC_PATH=$(node -pe '(".build/"+process.argv[1].slice(process.argv[1].indexOf("__e2e__"))).replace(".ts",".js")' $(realpath $3))
   fi
 
-  npx wdio configs/wdio.local.conf.js $2 $FIX_SPEC_PATH
+  npx wdio configs/wdio.browserstack.android.conf.js $2 $FIX_SPEC_PATH
 fi
--- lib/scripts/e2e.browserstack.ios.sh
+++ lib/scripts/e2e.browserstack.ios.sh
@@ -20,11 +20,11 @@ if [ "$1" == "--run" ]; then
 
   RESULT=$(curl -u "$BS_USER:$BS_KEY" -X POST "https://api-cloud.browserstack.com/app-automate/upload" -F "file=@tmp/ios/xcode-export/$IOS_PRODUCT.ipa")
 
-  export BS_PATH=$(node -pe 'JSON.parse(process.argv[1]).app_url' $RESULT)
+  export BS_PATH=$(node -pe 'JSON.parse(process.argv[1]).app_url' "$RESULT")
 
   if [ "$2" == "--spec" ]; then
     FIX_SPEC_PATH=$(node -pe '(".build/"+process.argv[1].slice(process.argv[1].indexOf("__e2e__"))).replace(".ts",".js")' $(realpath $3))
   fi
 
-  npx wdio configs/wdio.local.conf.js $2 $FIX_SPEC_PATH
+  npx wdio configs/wdio.browserstack.ios.conf.js $2 $FIX_SPEC_PATH
 fi
